0b5c7bdff7bda4fe043d7c46ebc0b161b1c6fcf2
lik <lik.24@yandex.ru>
2024-11-07 10:34:35 +0500

    up v1.1.3
    fix

--- test/testuf.jl
+++ test/testuf.jl
@@ -11,7 +11,7 @@ qw = rand(-1:0.1:1, nw, nt);
 qw[[1,3,7,9],:] .= -abs.(qw[[1,3,7,9],:]).-1;
 qw[[2,4,5,6,8],:] .= abs.(qw[[2,4,5,6,8],:].+1)
 
-rsl = sim_calc(qw = qw)
+rsl1 = sim_calc(qw = qw)
 AIW, _ = cIWC()
 
 uf = falses(nw, nt);
@@ -22,6 +22,6 @@ pw[[2,4,5,6,8],:] .= rsl1.pw[[2,4,5,6,8],:];
 rsl2 = sim_calc(qw = qw, uf = uf, pw = pw)
 @test (sum(abs,rsl1.pw.-rsl2.pw)<1e-3) & (sum(abs,rsl1.qw.-rsl2.qw)<1e-3)
 
-plt = plot(rsl.ppl[2,:])
-    plot!(plt, rsl.ppla[2,:])
-    plot!(plt, rsl.ppls[2,:])
+# plt = plot(rsl.ppl[2,:])
+#     plot!(plt, rsl.ppla[2,:])
+#     plot!(plt, rsl.ppls[2,:])
